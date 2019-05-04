82bb4a75ba32456d5ca4c758135dd240af8adf4b
johnmyf <johnmyf@163.com>
2019-05-04 12:43:59 +0800

    test normal merge2

--- helloworld.cpp
+++ helloworld.cpp
@@ -8,5 +8,6 @@ int main (int argc, char** argv){
 	cout << "hello world"<< endl;
 	cout << "here is just a test for conflict!"<< endl;
 	cout << "test to normal merge" << endl;
+	cout << "test to normal merge2" << endl;
 	return 0;
 }
